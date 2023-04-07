0ee07696fd11531751f5451eaaf21dd2a01fc63a
vinayakbora <vinayakbora09@gmail.com>
2023-04-07 11:27:03 +0530

    Implemented Location Service

--- app/src/main/java/com/example/todoapp/location/activity/LocationActivity.kt
+++ app/src/main/java/com/example/todoapp/location/activity/LocationActivity.kt
@@ -4,8 +4,9 @@ import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Address
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.todoapp.R
 import com.example.todoapp.databinding.ActivityLocationBinding
@@ -15,8 +16,8 @@ import com.google.android.gms.location.LocationServices
 
 class LocationActivity : AppCompatActivity() {
 
-    companion object{
-        fun openLocationActivity(ctx: Context){
+    companion object {
+        fun openLocationActivity(ctx: Context) {
             ctx.startActivity(Intent(ctx, LocationActivity::class.java))
         }
     }
@@ -31,37 +32,44 @@ class LocationActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_location)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-        FetchLocation.getLastLocation(this,
+        FetchLocation.getLastLocation(
+            this,
             this,
             fineLocation,
             coarseLocation,
-            binding.countryTextView,
-            binding.stateTextView,
-            binding.cityTextView,
-            binding.postalCodeTextView,
             fusedLocationClient
         )
 
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray,
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
             0 -> {
                 if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    FetchLocation.getLastLocation(this,
+                    FetchLocation.getLastLocation(
+                        this,
                         this,
                         fineLocation,
                         coarseLocation,
-                        binding.countryTextView,
-                        binding.stateTextView,
-                        binding.cityTextView,
-                        binding.postalCodeTextView,
                         fusedLocationClient
                     )
                 }
             }
         }
+    }
+
+    fun displayTextView(address: Address?) {
+        binding.countryTextView.text = address?.countryName
+        binding.stateTextView.text = address?.adminArea
+        binding.cityTextView.text = address?.locality
+        binding.postalCodeTextView.text = address?.postalCode
+        binding.latitudeTextView.text = address?.latitude.toString()
+        binding.longitudeTextView.text = address?.longitude.toString()
     }
 
 }
--- app/src/main/java/com/example/todoapp/utils/FetchLocation.kt
+++ app/src/main/java/com/example/todoapp/utils/FetchLocation.kt
@@ -4,26 +4,24 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.location.Location
-import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import com.example.todoapp.location.activity.LocationActivity
 import com.google.android.gms.location.FusedLocationProviderClient
 import java.util.*
 
 class FetchLocation {
-companion object{
-        fun getLastLocation(activity: LocationActivity,
-                            ctx: Context,
-                            fineLocation: String,
-                            coarseLocation: String,
-                            countryTextView: TextView,
-                            stateTextView: TextView,
-                            cityTextView: TextView,
-                            postalCodeTextView: TextView,
-                            fusedLocationClient: FusedLocationProviderClient) {
+    companion object {
+        fun getLastLocation(
+            activity: LocationActivity,
+            ctx: Context,
+            fineLocation: String,
+            coarseLocation: String,
+            fusedLocationClient: FusedLocationProviderClient,
+        ) {
             val permissions = arrayOf(fineLocation, coarseLocation)
-            if (ActivityCompat.checkSelfPermission(ctx, fineLocation) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(ctx, coarseLocation) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.checkSelfPermission(ctx,fineLocation) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(ctx,coarseLocation) != PackageManager.PERMISSION_GRANTED
+            ) {
                 ActivityCompat.requestPermissions(activity, permissions, 0)
             }
 
@@ -33,11 +31,8 @@ companion object{
                     val longitude = location.longitude
                     val geocoder = Geocoder(ctx, Locale.getDefault())
                     val addresses = geocoder.getFromLocation(latitude, longitude, 1)
-
-                    countryTextView.text = addresses?.get(0)?.countryName
-                    stateTextView.text = addresses?.get(0)?.adminArea
-                    cityTextView.text = addresses?.get(0)?.locality
-                    postalCodeTextView.text = addresses?.get(0)?.postalCode
+                    val geoResponse = addresses?.get(0)
+                    activity.displayTextView(geoResponse)
 
                 }
             }
